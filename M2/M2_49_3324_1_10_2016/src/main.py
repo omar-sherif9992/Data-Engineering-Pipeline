d6efe1a3b44aff413a35ea2d924b8e574a7f06b7
omar-sherif9992 <omar.sherif9992.com>
2023-11-13 04:13:27 +0200

    python preprocessing docker image done

--- M2/M2_49_3324_1_10_2016/src/main.py
+++ M2/M2_49_3324_1_10_2016/src/main.py
@@ -1,4 +1,5 @@
 import os
+from ingestion import ingest
 
 ROOT_DIR = os.path.abspath(".")
 DATASET_DIR = f"{ROOT_DIR}/data"  # Directory for storing datasets.
@@ -11,9 +12,9 @@ lookup_table_file_name = "lookup_table_green_taxis.csv"
 
 if __name__ == "__main__":
     cleaned_file_path = os.path.join(DATASET_DIR, cleaned_dataset_file_name)
-    cleaned_file_path = os.path.join(DATASET_DIR, lookup_table_file_name)
+    lookup_table_file_path = os.path.join(DATASET_DIR, lookup_table_file_name)
     
-    if not os.path.exists(cleaned_file_path) or not os.path.exists(lookup_table_file_name):
+    if not os.path.exists(cleaned_file_path) or not os.path.exists(lookup_table_file_path):
         print("Preprocessing the dataset...")
         try:
             print("Trying to run python3...")
@@ -22,5 +23,10 @@ if __name__ == "__main__":
             print("Trying to run python...")
             os.system("python src/preprocessing.py")
         print("Preprocessing completed.")
+    
+    print("Ingesting the dataset...")
+    
+    ingest([cleaned_file_path, lookup_table_file_path], ["green_taxi_10_2016", "lookup_green_taxi_10_2016"])
+    
         
     
